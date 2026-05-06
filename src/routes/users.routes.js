845f04df5f374951ff34a77ba292ec797a8ea99b
mradul-verma <mridulv06@gmail.com>
2026-05-07 01:01:26 +0530

    initial commit

---
+++ src/routes/users.routes.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import {
+  addToHistory,
+  getUserHistory,
+  login,
+  register,
+} from "../controllers/user.controller.js";
+
+const router = Router();
+
+router.route("/login").post(login);
+router.route("/register").post(register);
+router.route("/add_to_activity").post(addToHistory);
+router.route("/get_all_activity").get(getUserHistory);
+
+export default router;
+
+import dotenv from "dotenv";
+dotenv.config();
